2e4ca447d75c90ccc00f3b48bbc1b7fb930054d2
Praneeth_vvs <spraneeth4@gmail.com>
2024-12-01 12:31:18 -0600

    added lambda authorizer policy statement

--- lambda_apigw_cognito/api_iac/assets/authorizer/authorizer.py
+++ lambda_apigw_cognito/api_iac/assets/authorizer/authorizer.py
@@ -1,12 +1,23 @@
 import json
 import os
+import boto3 
+
+ssm_client = boto3.client('ssm')
 
 def handler(event, context):
     # Extract the token (usually from Authorization header)
+    print(event)
     token = event['authorizationToken']
     
     # Get the API Gateway URL from environment variable
-    api_gateway_url = os.getenv('API_GATEWAY_URL')
+
+    parameter_name = "/DocQuerySystemRAGApp/apigw_resource_arn"
+    response = ssm_client.get_parameter(
+                Name=parameter_name,
+                WithDecryption=True  # Set to True if the parameter is encrypted (SecureString)
+            )
+    resource_arn = response['Parameter']['Value']
+    print(f"Retrieved parameter value: {resource_arn}")
 
 
     # If the token is valid, create an allow policy, otherwise deny
@@ -23,12 +34,12 @@ def handler(event, context):
                 {
                     'Effect': f'{effect}',
                     'Action': 'execute-api:Invoke',
-                    'Resource': f'{api_gateway_url}/*'  # Use the API Gateway URL in the policy
+                    'Resource': [f'{resource_arn.rstrip("/")}'] # Use the API Gateway URL in the policy
                 }
             ]
         }
     }
 
    
-
+    print(policy)
     return policy
